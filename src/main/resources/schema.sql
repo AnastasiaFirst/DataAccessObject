0e88363970d68e37b929f3d8722ea0304c659629
AnastasiaFirst <92177620+AnastasiaFirst@users.noreply.github.com>
2025-03-04 20:50:43 +0300

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,16 @@
-CREATE TABLE CUSTOMERS (
-    id INT AUTO_INCREMENT,
+CREATE TABLE netology.CUSTOMERS (
+    id SERIAL PRIMARY KEY,
     name VARCHAR(50),
     surname VARCHAR(50),
     age INT,
-    phone_number VARCHAR(15),
-    PRIMARY KEY (id)
+    phone_number VARCHAR(15)
 );
 
-CREATE TABLE ORDERS (
-    id INT AUTO_INCREMENT,
+CREATE TABLE netology.ORDERS (
+    id SERIAL PRIMARY KEY,
     date DATE,
     customer_id INT,
     product_name VARCHAR(100),
     amount DECIMAL(10, 2),
-    PRIMARY KEY (id),
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(id)
+    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(id) ON DELETE CASCADE
 );
